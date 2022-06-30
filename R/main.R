0040252297b8c488ae1657004f4f8e1a6efcbf7d
Prajwal Patil <prajwalpatilk@gmail.com>
2022-06-30 10:11:51 +0530

    Add jd functions

--- R/main.R
+++ R/main.R
@@ -10,15 +10,15 @@ data(SE)
 
 
 moon_longitude <- function(jd){
-  return (swe_calc_ut(jd, SE$MOON, SE$FLG_SWIEPH)$xx[1]);
+  return (swe_calc_ut(jd, SE$MOON, SE$FLG_SWIEPH)$xx[1])
 }
 
 sun_longitude <- function(jd){
-  return (swe_calc_ut(jd, SE$SUN, SE$FLG_SWIEPH)$xx[1]);
+  return (swe_calc_ut(jd, SE$SUN, SE$FLG_SWIEPH)$xx[1])
 }
 
 from_dms <- function(degs,mins,secs){
-  return (degs + mins/60 + secs/3600);
+  return (degs + mins/60 + secs/3600)
 }
 
 to_dms <- function(deg){
@@ -26,7 +26,47 @@ to_dms <- function(deg){
   mins = (deg - d) * 60
   m = as.integer(mins)
   s = as.integer(round((mins - m) * 60))
-  return (c(d, m, s));
+  return (c(d, m, s))
 }
 
+unwrap_angles <- function(angles){
+  angles_unwraped = angles
+  for(i in 2:length(angles)){
+    if(angles_unwraped[i] < angles_unwraped[i-1]){
+      angles_unwraped[i] <- angles_unwraped[i] + 360
+    }
+  }
+  if(is.unsorted(angles_unwraped)){
+    print("unwrap_angles : angles are not sorted!")
+  }
+  return (angles_unwraped)
+}
+
+
+inverse_lagrange <- function(x,y,ya){
+  if(length(x) != length(y)){
+    print("Lengths are not equal!")
+  }
+  total <- 0
+  for(i in 1:length(x)){
+    numerator = 1
+    denominator = 1
+    for(j in 1:length(x)){
+      if(j != i){
+        numerator = numerator * (ya - y[j])
+        denominator = denominator * (y[i] - y[j])
+      }
+    }
+    total <- total + ((numerator * x[i]) / denominator)
+  }
+  return (total)
+}
+
+gregorian_to_jd <- function(day,month,year){
+  return (swe_julday(year, month, day, 0.0,SE$GREG_CAL))
+}
+
+jd_to_gregorian <- function(jd){
+  return (swe_revjul(jd, SE$GREG_CAL))
+}
 
