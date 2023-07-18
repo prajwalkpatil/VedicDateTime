509b39ff5147e05b0885ee06538d71c549f5528b
Prajwal Patil <prajwalpatilk@gmail.com>
2023-07-18 10:22:14 +0530

    Fixed bug of multiple ritus in the case of Adhika Masa

--- tests/testthat/test_ritu.R
+++ tests/testthat/test_ritu.R
@@ -6,5 +6,5 @@ source_test_helpers()
 masa_num <- masa(vd$jd, vd$place)
 
 test_that("Check positive testcase", {
-  expect_equal(ritu(masa_num), c(2, 0), tolerance = .Machine$double.eps^0.4)
+  expect_equal(ritu(masa_num), 2, tolerance = .Machine$double.eps^0.4)
 })
